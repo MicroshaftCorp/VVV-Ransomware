8d0a27deeac412f561207efc477f3b4d6fc881f4
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-14 07:24:34 -0700

    Create VVV-Decrypter.bat

---
+++ VVV-Decrypter.bat
@@ -0,0 +1,9 @@
+del VVV-Encrypter.bat
+color 47
+title Decrypter
+timeout 9
+echo VVV-Encrypter Sucessfully Deleted!
+ren if file is encrypted,instead close the window then delete it.
+echo Do You Wanna Restart?
+pause
+shutdown -r -t 10 -c "Virus Attack..."
